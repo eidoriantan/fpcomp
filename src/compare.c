d31bab841fb5d16945f98fa4e34bca792e971d78
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2024-08-26 23:06:24 +0800

    Replace tabs with spaces

--- src/compare.c
+++ src/compare.c
@@ -16,112 +16,112 @@ const uint64_t M4 = 0x0f0f0f0f0f0f0f0fULL;
 const uint64_t H01 = 0x0101010101010101ULL;
 
 int popcount (uint64_t x) {
-	x -= (x >> 1) & M1;
-	x = (x & M2) + ((x >> 2) & M2);
-	x = (x + (x >> 4)) & M4;
-	return (x * H01) >> 56;
+  x -= (x >> 1) & M1;
+  x = (x & M2) + ((x >> 2) & M2);
+  x = (x + (x >> 4)) & M4;
+  return (x * H01) >> 56;
 }
 
 float match_fingerprints (signed int *a, int asize, signed int *b, int bsize, int maxoffset) {
-	int i, topcount, topoffset, size, biterror, minsize, auniq = 0, buniq = 0;
-	int numcounts = asize + bsize + 1;
-	unsigned short *counts = calloc(numcounts, sizeof(unsigned short));
-	uint8_t *seen;
-	uint16_t *aoffsets, *boffsets;
-	uint64_t *adata, *bdata;
-	float score, diversity;
+  int i, topcount, topoffset, size, biterror, minsize, auniq = 0, buniq = 0;
+  int numcounts = asize + bsize + 1;
+  unsigned short *counts = calloc(numcounts, sizeof(unsigned short));
+  uint8_t *seen;
+  uint16_t *aoffsets, *boffsets;
+  uint64_t *adata, *bdata;
+  float score, diversity;
 
-	aoffsets = calloc((MATCH_MASK + 1) * 2, sizeof(uint16_t));
-	boffsets = aoffsets + MATCH_MASK + 1;
-	seen = (uint8_t *)aoffsets;
+  aoffsets = calloc((MATCH_MASK + 1) * 2, sizeof(uint16_t));
+  boffsets = aoffsets + MATCH_MASK + 1;
+  seen = (uint8_t *)aoffsets;
 
-	for (i = 0; i < asize; i++) {
-		aoffsets[MATCH_STRIP(a[i])] = i;
-	}
+  for (i = 0; i < asize; i++) {
+    aoffsets[MATCH_STRIP(a[i])] = i;
+  }
 
-	for (i = 0; i < bsize; i++) {
-		boffsets[MATCH_STRIP(b[i])] = i;
-	}
+  for (i = 0; i < bsize; i++) {
+    boffsets[MATCH_STRIP(b[i])] = i;
+  }
 
-	topcount = 0;
-	topoffset = 0;
-	for (i = 0; i < MATCH_MASK; i++) {
-		if (aoffsets[i] && boffsets[i]) {
-			int offset = aoffsets[i] - boffsets[i];
-			if (maxoffset == 0 || (-maxoffset <= offset && offset <= maxoffset)) {
-				offset += bsize;
-				counts[offset]++;
-				if (counts[offset] > topcount) {
-					topcount = counts[offset];
-					topoffset = offset;
-				}
-			}
-		}
-	}
+  topcount = 0;
+  topoffset = 0;
+  for (i = 0; i < MATCH_MASK; i++) {
+    if (aoffsets[i] && boffsets[i]) {
+      int offset = aoffsets[i] - boffsets[i];
+      if (maxoffset == 0 || (-maxoffset <= offset && offset <= maxoffset)) {
+        offset += bsize;
+        counts[offset]++;
+        if (counts[offset] > topcount) {
+          topcount = counts[offset];
+          topoffset = offset;
+        }
+      }
+    }
+  }
 
-	topoffset -= bsize;
+  topoffset -= bsize;
 
-	minsize = MIN(asize, bsize) & ~1;
-	if (topoffset < 0) {
-		b -= topoffset;
-		bsize = MAX(0, bsize + topoffset);
-	}
-	else {
-		a += topoffset;
-		asize = MAX(0, asize - topoffset);
-	}
+  minsize = MIN(asize, bsize) & ~1;
+  if (topoffset < 0) {
+    b -= topoffset;
+    bsize = MAX(0, bsize + topoffset);
+  }
+  else {
+    a += topoffset;
+    asize = MAX(0, asize - topoffset);
+  }
 
-	size = MIN(asize, bsize) / 2;
-	if (!size || !minsize) {
-		printf("acoustid_compare: empty matching subfingerprint\n");
-		score = 0.0;
-		goto exit;
-	}
+  size = MIN(asize, bsize) / 2;
+  if (!size || !minsize) {
+    printf("acoustid_compare: empty matching subfingerprint\n");
+    score = 0.0;
+    goto exit;
+  }
 
-	memset(seen, 0, UNIQ_MASK);
-	for (i = 0; i < asize; i++) {
-		int key = UNIQ_STRIP(a[i]);
-		if (!seen[key]) {
-			auniq++;
-			seen[key] = 1;
-		}
-	}
+  memset(seen, 0, UNIQ_MASK);
+  for (i = 0; i < asize; i++) {
+    int key = UNIQ_STRIP(a[i]);
+    if (!seen[key]) {
+      auniq++;
+      seen[key] = 1;
+    }
+  }
 
-	memset(seen, 0, UNIQ_MASK);
-	for (i = 0; i < bsize; i++) {
-		int key = UNIQ_STRIP(b[i]);
-		if (!seen[key]) {
-			buniq++;
-			seen[key] = 1;
-		}
-	}
+  memset(seen, 0, UNIQ_MASK);
+  for (i = 0; i < bsize; i++) {
+    int key = UNIQ_STRIP(b[i]);
+    if (!seen[key]) {
+      buniq++;
+      seen[key] = 1;
+    }
+  }
 
-	diversity = MIN(MIN(1.0, (float)(auniq + 10) / asize + 0.5),
-	                MIN(1.0, (float)(buniq + 10) / bsize + 0.5));
+  diversity = MIN(MIN(1.0, (float)(auniq + 10) / asize + 0.5),
+                  MIN(1.0, (float)(buniq + 10) / bsize + 0.5));
 
-	if (topcount < MAX(auniq, buniq) * 0.02) {
-		printf("acoustid_compare: top offset score is below 2%% of the unique size\n");
-		score = 0.0;
-		goto exit;
-	}
+  if (topcount < MAX(auniq, buniq) * 0.02) {
+    printf("acoustid_compare: top offset score is below 2%% of the unique size\n");
+    score = 0.0;
+    goto exit;
+  }
 
-	adata = (uint64_t *)a;
-	bdata = (uint64_t *)b;
-	biterror = 0;
-	for (i = 0; i < size; i++, adata++, bdata++) {
-		biterror += popcount(*adata ^ *bdata);
-	}
-	score = (size * 2.0 / minsize) * (1.0 - 2.0 * (float)biterror / (64 * size));
-	if (score < 0.0) {
-		score = 0.0;
-	}
-	if (diversity < 1.0) {
-		float newscore = pow(score, 8.0 - 7.0 * diversity);
-		printf("acoustid_compare2: scaling score because of duplicate items, %f => %f\n", score, newscore);
-		score = newscore;
-	}
+  adata = (uint64_t *)a;
+  bdata = (uint64_t *)b;
+  biterror = 0;
+  for (i = 0; i < size; i++, adata++, bdata++) {
+    biterror += popcount(*adata ^ *bdata);
+  }
+  score = (size * 2.0 / minsize) * (1.0 - 2.0 * (float)biterror / (64 * size));
+  if (score < 0.0) {
+    score = 0.0;
+  }
+  if (diversity < 1.0) {
+    float newscore = pow(score, 8.0 - 7.0 * diversity);
+    printf("acoustid_compare2: scaling score because of duplicate items, %f => %f\n", score, newscore);
+    score = newscore;
+  }
 exit:
-	free(aoffsets);
-	free(counts);
-	return score;
+  free(aoffsets);
+  free(counts);
+  return score;
 }
--- src/main.c
+++ src/main.c
@@ -5,25 +5,25 @@
 #include "compare.h"
 
 int main (int argc, char *argv[]) {
-	if (argc != 3) {
-		printf("Invalid number of arguments\n");
-		return 1;
-	}
+  if (argc != 3) {
+    printf("Invalid number of arguments\n");
+    return 1;
+  }
 
-	int32_t *a = NULL;
-	int32_t *b = NULL;
-	size_t a_len, b_len;
+  int32_t *a = NULL;
+  int32_t *b = NULL;
+  size_t a_len, b_len;
 
-	read_fp(&a, &a_len, argv[1]);
-	read_fp(&b, &b_len, argv[2]);
+  read_fp(&a, &a_len, argv[1]);
+  read_fp(&b, &b_len, argv[2]);
 
-	if (a != NULL && b != NULL) {
-		float score = match_fingerprints(a, a_len, b, b_len, 80);
-		printf("%f\n", score);
-	} else {
-		printf("Unable to read input files\n");
-		return 1;
-	}
+  if (a != NULL && b != NULL) {
+    float score = match_fingerprints(a, a_len, b, b_len, 80);
+    printf("%f\n", score);
+  } else {
+    printf("Unable to read input files\n");
+    return 1;
+  }
 
-	return 0;
+  return 0;
 }
